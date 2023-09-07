276801b392728b27b82f48ab90afe19a9027d4ed
Elias Fonseca <contato@ignitor.com.br>
2023-09-07 13:53:45 -0300

    feat(auth): create routes and controllers for auth and list

--- routes/api.php
+++ routes/api.php
@@ -1,8 +1,22 @@
 <?php
 
-use Illuminate\Http\Request;
+use App\Http\Controllers\Api\AuthController;
+use App\Http\Controllers\Api\ListController;
 use Illuminate\Support\Facades\Route;
 
-Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
+Route::prefix('v1')->group(function () {
+    Route::controller(AuthController::class)->group(function () {
+        Route::post('/register','register')->name('register');
+        Route::post('/login', 'login')->name('login');
+    });
+
+    Route::middleware(['auth:api'])->group(function () {
+        Route::controller(ListController::class)->group(function () {
+            Route::get('/lists', 'index');
+            Route::get('/lists/{id}', 'show');
+            Route::post('/lists', 'store');
+            Route::put('/lists', 'update');
+            Route::delete('/lists', 'delete');
+        });
+    });
 });
